d26b7cdd193214062e53b23dd6df214cdd9595a9
Murat Ugur Eminoglu <murat@murat.ws>
2024-01-26 16:56:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $status_data = curl_exec($curl);
 $status_data_array = json_decode($status_data, true);
 
 if ($status_data_array && isset($status_data_array['statusCode']) && $status_data_array['statusCode'] == 500) {
-    echo "run create_url";
     $create_curl = curl_init();
     curl_setopt($create_curl, CURLOPT_URL, $create_url);
     curl_setopt($create_curl, CURLOPT_RETURNTRANSFER, true);
